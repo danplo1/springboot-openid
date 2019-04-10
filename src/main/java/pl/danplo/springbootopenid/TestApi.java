d008e9c77bb24a926f2bd2258a8612706cfcdd77
danplo1 <budweiser401@wp.pl>
2019-04-10 14:25:39 +0200

    Simple Single Sign + OpenID_OAuth2 ver for Google

--- src/main/java/pl/danplo/springbootopenid/TestApi.java
+++ src/main/java/pl/danplo/springbootopenid/TestApi.java
@@ -1,10 +1,18 @@
 package pl.danplo.springbootopenid;
 
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.LinkedHashMap;
+
+@Controller
 public class TestApi {
 
     @GetMapping("/test1")
@@ -15,9 +23,26 @@ public class TestApi {
 
 
     @GetMapping("/test2")
-    public String test2() {
+    public String test2(Model model) {
 
-        return "test2";
+        Object details = ((UsernamePasswordAuthenticationToken)
+                ((OAuth2Authentication) ((SecurityContextImpl) SecurityContextHolder
+                        .getContext())
+                        .getAuthentication())
+                        .getUserAuthentication())
+                        .getDetails();
+
+        String name = ((LinkedHashMap) details).values().toArray()[1].toString();
+        String picture = ((LinkedHashMap) details).values().toArray()[4].toString();
+        String email = ((LinkedHashMap) details).values().toArray()[5].toString();
+
+
+        model.addAttribute("name", name);
+        model.addAttribute("picture", picture);
+        model.addAttribute("email", email);
+
+
+        return "gui";
     }
 
 }
